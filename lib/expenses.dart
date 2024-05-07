a1507c22b766bced82aeb0174afaba5f280579bd
CharlesCoeder <croederdev@gmail.com>
2024-05-06 20:45:03 -0400

    Show expenses in a list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,13 +12,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "KFC",
+        amount: 10.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: "Knicks vs Pacers Game 1",
+        amount: 300,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text("Chart"),
-          Text('Expenses List...'),
+          const Text("Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );
