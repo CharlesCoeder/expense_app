0e02c82e49bcf93f83f5758ce38f47de19a3b12c
CharlesCoeder <croederdev@gmail.com>
2024-05-06 20:52:36 -0400

    Style expenses with ExpenseItem widget

---
+++ lib/expenses_list/expense_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 20,
+        ),
+        child: Text(expense.title),
+      ),
+    );
+  }
+}
