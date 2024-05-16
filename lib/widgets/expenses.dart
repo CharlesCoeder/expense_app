cb4bd534824a990652ddaa36eae0e639adbe8606
CharlesCoeder <croederdev@gmail.com>
2024-05-16 02:26:56 -0400

    Placeholder text when there are no expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -50,6 +50,19 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses yet. Press + to add an expense!"),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = Expanded(
+        child: ExpensesList(
+          expenses: _registeredExpenses,
+          onRemoveExpense: _removeExpense,
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense Tracker"),
@@ -61,12 +74,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text("Chart"),
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
-          )
+          mainContent,
         ],
       ),
     );
