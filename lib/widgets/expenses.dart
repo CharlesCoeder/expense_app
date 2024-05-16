0dfdeea4897668e5546f6b6642db385b531d2852
CharlesCoeder <croederdev@gmail.com>
2024-05-16 02:20:47 -0400

    Remove expenses with sliding gestures

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,6 +29,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   final List<Expense> _registeredExpenses = [
     Expense(
         title: "KFC",
@@ -55,7 +61,12 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text("Chart"),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
+          )
         ],
       ),
     );
