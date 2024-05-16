fd252a3c80f3e9a58249b361736c6c392e9a9c4f
CharlesCoeder <croederdev@gmail.com>
2024-05-16 05:20:52 -0400

    Theming for swipe delete gesture

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,19 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.7),
+            margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+              vertical: Theme.of(context).cardTheme.margin!.vertical,
+            ),
+            child: const Row(children: [
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: Icon(Icons.delete),
+              ),
+            ])),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
