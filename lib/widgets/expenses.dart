f75c05102202157ee9571cfc4cc04b46e2964be4
CharlesCoeder <croederdev@gmail.com>
2024-05-16 02:09:15 -0400

    Save and display new expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -14,7 +14,18 @@ class Expenses extends StatefulWidget {
 
 class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   final List<Expense> _registeredExpenses = [
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
 
+  final void Function(Expense expense) onAddExpense;
   @override
   State<NewExpense> createState() {
     return _NewExpenseState();
@@ -40,6 +41,13 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(Expense(
+      title: _titleController.text,
+      amount: enteredAmount,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    ));
+    Navigator.pop(context);
   }
 
   void _showDatePicker() async {
